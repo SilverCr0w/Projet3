015baaaa195486b40a0f475e3276119f21a0efc0
SilverCr0w <donavanvasnier@gmail.com>
2016-11-21 14:29:16 +0100

    Séparation du header en un fichier php

---
+++ Projet3header.php
@@ -0,0 +1,75 @@
+		<header>
+<nav class="navbar navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.html"> <img src="logo_pyramide_accueil.png"><div id="reserv">Réservation<br/><div id="dep">Musée du Louvre</div></div></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="menu">
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.html"><i class="fa fa-home"></i> Accueil</a></li>
+        <li class="dropdown"> 
+            <a class="passion" data-toggle="dropdown" href="#"><i class="fa fa-info"></i> Informations pratiques<b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="#"><i class="fa fa-clock-o"></i> Horaires</a></li>
+              <li><a href="#"><i class="fa fa-money"></i> Tarifs</a></li>
+              <li><a href="#"><i class="fa fa-calendar"></i> Ouverture des salles</a></li>
+            </ul>
+          </li>
+        <li><a href="#experience"><i class="fa fa-shopping-cart"></i> Billeterie</a></li>
+            
+      </ul>
+    </div>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+	</header>
+
+	<section>
+		<article>
+			
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+ 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="image1.jpg" alt="...">
+     
+    </div>
+    <div class="item">
+      <img src="image2.jpg" alt="...">
+      
+    </div>
+    <div class="item">
+      <img src="image3.jpg" alt="...">
+      
+    </div>
+  </div>
+ 
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div> <!-- Carousel -->
+		</article>
+	</section>
+
+  
